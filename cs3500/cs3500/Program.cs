1c7c3f2bcef68e4cd4289903a243f53abbe628fb
Xiaoyun <xiaoyunding012@gmail.com>
2015-09-02 13:10:30 -0600

    add methods a and b

---
+++ cs3500/cs3500/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs3500
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void a()
+        {
+
+        }
+
+        static void b(int x)
+        {
+
+        }
+    }
+}
